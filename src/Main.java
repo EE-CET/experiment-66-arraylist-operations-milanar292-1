603b7a9933565857275af3f6e71050ee23c0816a
milanar292 <milanar292@gmail.com>
2026-04-10 00:24:24 +0530

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        ArrayList<String> cart = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            cart.add(sc.next());
+        }
+
+        String searchItem = sc.next();
+
+        Collections.sort(cart);
+        System.out.println("Sorted Items: " + cart);
+
+        if (cart.contains(searchItem))
+            System.out.println("Found");
+        else
+            System.out.println("Not Found");
+
+        sc.close();
+    }
+}
