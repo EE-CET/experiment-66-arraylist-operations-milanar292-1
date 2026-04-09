f06582ff8fab13a699466c6d5b3cb277be971dc1
milanar292 <milanar292@gmail.com>
2026-04-09 23:59:35 +0530

    Update ArrayListOperations.java

--- src/ArrayListOperations.java
+++ src/ArrayListOperations.java
@@ -1,26 +1,23 @@
 import java.util.*;
 
-public class ArrayListOperations {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         int n = sc.nextInt();
         ArrayList<String> cart = new ArrayList<>();
         
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             cart.add(sc.next());
         }
         
         String searchItem = sc.next();
         
-        // Sort alphabetically
         Collections.sort(cart);
         
-        // Print sorted list
         System.out.println("Sorted Items: " + cart);
         
-        // Check if item exists
-        if(cart.contains(searchItem)) {
+        if (cart.contains(searchItem)) {
             System.out.println("Found");
         } else {
             System.out.println("Not Found");
